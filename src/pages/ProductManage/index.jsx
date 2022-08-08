a2bda17f741113677c1ebbcd49a6d0fff0f978fa
KavinduMihiranga <mihirangak1999@gmail.com>
2022-08-08 15:36:04 +0530

    addedCartTable

--- src/pages/ProductManage/index.jsx
+++ src/pages/ProductManage/index.jsx
@@ -1,18 +1,18 @@
 import React, {Component, Fragment} from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
 import {TextValidator, ValidatorForm} from "react-material-ui-form-validator";
-import user from "../../assets/img/user.png"
-import InputAdornment from "@mui/material/InputAdornment";
-import Button from "@mui/material/Button";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 import GDSEButton from "../../components/Button";
+import InputAdornment from "@mui/material/InputAdornment";
+import user from "../../assets/img/user.png"
 import TableContainer from "@mui/material/TableContainer";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import TableHead from "@mui/material/TableHead";
 import TableBody from "@mui/material/TableBody";
 import Tooltip from "@mui/material/Tooltip";
 import IconButton from "@mui/material/IconButton";
@@ -20,11 +20,10 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ProductServices from "../../service/ProductServices";
 import GDSESnackBar from "../../components/SnackBar";
-import Autocomplete from "@mui/material/Autocomplete";
-import {Category} from "@mui/icons-material";
 class ProductManage extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
             formData: {
                 title: "",
@@ -53,13 +52,10 @@ class ProductManage extends Component {
                 { label: 'Spicy' },
             ]
         }
-
-
-
     }
-    deleteProduct = async (pid) => {
+    deleteProduct = async (id) => {
         let params = {
-            pid: pid
+            id: id
         }
         let res = await ProductServices.deleteProduct(params);
         if (res.status === 200) {
@@ -93,7 +89,6 @@ class ProductManage extends Component {
             }
         })
     };
-
     clearFields = (e) => {
         this.setState({
             formData: {
@@ -105,13 +100,15 @@ class ProductManage extends Component {
 
             }
         });
+
+        console.log("clear button click")
     }
 
     submitProduct = async () => {
         let formData = this.state.formData;
         if (this.state.btnLabel === "Save") {
             let res = await ProductServices.postProduct(formData);
-            console.log(res);
+            console.log("Submit button click"+res.data);
 
             if (res.status === 201) {
                 this.setState({
@@ -160,16 +157,13 @@ class ProductManage extends Component {
         }
         console.log(this.state.data)
     }
-
     componentDidMount() {
-        this.loadData();
+            this.loadData();
 
-        console.log(this.state.data)
-    }
-    render() {
-        const { classes } = this.props;
-        return (
-
+            console.log(this.state.data)
+        }
+render() {
+        return(
             <Fragment>
                 <Grid  container className="pt-7" spacing={3} style={{backgroundColor:"#d9d9d9"}}>
                     <Grid item xs={12} sm={12} md={12} lg={12}>
@@ -177,15 +171,14 @@ class ProductManage extends Component {
                             <Typography variant="h4">Product Management</Typography>
                         </div>
                     </Grid>
-
                 </Grid>
-
                 <ValidatorForm
                     ref="form"
                     className="pt-2"
-                    onSubmit={this.submitProduct()}
+                    // onSubmit={this.submitProduct()}
 
                 >
+
                     <Grid container className="pt-2" spacing={3} style={{backgroundColor:"#d9d9d9"}}>
                         <Grid item xs={12} sm={12} md={6} lg={6} >
                             <TextValidator
@@ -218,95 +211,80 @@ class ProductManage extends Component {
                             />
                         </Grid>
                         <Grid item xs={12} sm={12} md={6} lg={6} >
-                            {/*<TextValidator*/}
-                            {/*    id="outlinedbasic"*/}
-                            {/*    placeholder="Category"*/}
-                            {/*    variant="outlined"*/}
-                            {/*    size="small"*/}
-                            {/*    style={{width: '100%',backgroundColor:"white"}}*/}
-                            {/*    value={this.state.formData.category}*/}
-                            {/*    onChange={(e) => {*/}
-                            {/*        let formData = this.state.formData*/}
-                            {/*        formData.category = e.target.value*/}
-                            {/*        this.setState({formData})*/}
-                            {/*    }}*/}
-                            {/*/>*/}
 
-
-                            <Autocomplete
-                                disablePortal
-                                id="combo-box-demo"
-                                options={this.state.category}
-                                sx={{ width: 300 }}
-                                style={{width: '100%',backgroundColor:"white"}}
-                                renderInput={(params) => <TextField {...params} label="Category" />}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={6} lg={6} style={{height:150}}>
-                            <TextValidator
-                                id="outlinedbasic"
-                                placeholder="Description"
-                                // variant="elevation"
-                                size="small"
-                                style={{width: '100%',backgroundColor:"white"}}
-                                value={this.state.formData.description}
-                                onChange={(e) => {
-                                    let formData = this.state.formData
-                                    formData.description = e.target.value
-                                    this.setState({formData})
-                                }}
-                            />
-                        </Grid>
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={this.state.category}
+                            sx={{ width: 300 }}
+                            style={{width: '100%',backgroundColor:"white"}}
+                            renderInput={(params) => <TextField {...params} label="Category" />}
+                        />
                     </Grid>
+
+                    <Grid item xs={12} sm={12} md={6} lg={6} style={{height:150}}>
+                        <TextValidator
+                            id="outlinedbasic"
+                            placeholder="Description"
+                            // variant="elevation"
+                            size="small"
+                            style={{width: '100%',backgroundColor:"white"}}
+                            value={this.state.formData.description}
+                            onChange={(e) => {
+                                let formData = this.state.formData
+                                formData.description = e.target.value
+                                this.setState({formData})
+                            }}
+                        />
+                    </Grid>
+
+                    </Grid>
+
+                    <Grid  container className="pt-2" spacing={3} style={{backgroundColor:"#d9d9d9"}}>
+                        <Grid item xs={5} sm={5} md={5} lg={5} >
+                            <div sx={{maxWidth: 120}}>
+                                {/*{console.log(this.handleChange.event)}*/}
+                                {/*<img src={this.handleChange.event} alt=""/>*/}
+                                <img src={user} alt=""/>
+                            </div>
+                            <TextField
+                                id="input-with-icon-textfield"
+                                label="Add Product"
+                                type={'file'}
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+
+                                        </InputAdornment>
+                                    ),
+                                }}
+                                // onClick={this.setState.file}
+                                variant="standard"
+                            />
+                        </Grid>
+
+
+                    </Grid>
+
 
                     <Grid container style={{marginTop: 150}} direction="row" justifyContent="flex-end"
                           alignItems="center" style={{backgroundColor:"#d9d9d9"}}
-                          // onClick={this.clearFields}
+                        // onClick={this.clearFields}
                     >
                         <GDSEButton  size="bif" label={"Clear"}
                                      variant="contained"
-
+                                     onClick={this.clearFields}
                                      style={{marginRight: 20,backgroundColor:"#98801C"}}
                         />
                         <GDSEButton type="submit" size="big" label={"Save"}
                                     style={{marginRight: 20}}
-                                    // onClick={this.clearFields}
+                            onClick={this.submitProduct}
+
                                     variant="contained"/>
                     </Grid>
 
                 </ValidatorForm>
-
-
-                <Grid  container className="pt-2" spacing={3} style={{backgroundColor:"#d9d9d9"}}>
-                    <Grid item xs={5} sm={5} md={5} lg={5} >
-                        <div sx={{maxWidth: 120}}>
-                            {/*{console.log(this.handleChange.event)}*/}
-                            {/*<img src={this.handleChange.event} alt=""/>*/}
-                            <img src={user} alt=""/>
-                        </div>
-                        <TextField
-                            id="input-with-icon-textfield"
-                            label="Add Product"
-                            type={'file'}
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-
-                                    </InputAdornment>
-                                ),
-                            }}
-                            // onClick={this.setState.file}
-                            variant="standard"
-                        />
-                    </Grid>
-
-
-                </Grid>
-
-
-
                 <Grid container style={{marginTop: '10px'}}>
-
                     <TableContainer component={Paper}>
                         <Table sx={{minWidth: 650}} aria-label="user table">
                             <TableHead>
@@ -319,43 +297,43 @@ class ProductManage extends Component {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {
-                                  this.state.date.map((row)=>(
-                                <TableRow>
-                                    <TableCell align="right">{row.title}</TableCell>
-                                    <TableCell align="right">{row.price}</TableCell>
-                                    <TableCell align="right">{row.category}</TableCell>
-                                    <TableCell align="right">{row.description}</TableCell>
+                                {/*{*/}
+                                {/*    this.state.date.map((row)=>(*/}
+                                        <TableRow>
+                                            <TableCell align="right">row.title}</TableCell>
+                                            <TableCell align="right">row.price}</TableCell>
+                                            <TableCell align="right">row.category}</TableCell>
+                                            <TableCell align="right">row.description}</TableCell>
 
 
-                                    <TableCell align="right">
-                                        <Tooltip title="Edit">
-                                            <IconButton
-                                                onClick={() => {
-                                                    console.log("edit icon clicked!")
-                                                    this.updateProduct();
-                                                }}
-                                            >
-                                                <EditIcon color={"primary"}/>
-                                            </IconButton>
-                                        </Tooltip>
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        <Tooltip title="Delete">
-                                            <IconButton
-                                                onClick={() => {
-                                                    console.log("delete icon clicked!")
-                                                    this.deleteProduct();
-                                                }}
-                                            >
-                                                <DeleteIcon color={"error"}/>
-                                            </IconButton>
-                                        </Tooltip>
-                                    </TableCell>
+                                            <TableCell align="right">
+                                                <Tooltip title="Edit">
+                                                    <IconButton
+                                                        onClick={() => {
+                                                            console.log("edit icon clicked!")
+                                                            this.updateProduct();
+                                                        }}
+                                                    >
+                                                        <EditIcon color={"primary"}/>
+                                                    </IconButton>
+                                                </Tooltip>
+                                            </TableCell>
+                                            <TableCell align="right">
+                                                <Tooltip title="Delete">
+                                                    <IconButton
+                                                        onClick={() => {
+                                                            console.log("delete icon clicked!")
+                                                            this.deleteProduct();
+                                                        }}
+                                                    >
+                                                        <DeleteIcon color={"error"}/>
+                                                    </IconButton>
+                                                </Tooltip>
+                                            </TableCell>
 
-                                </TableRow>
-                                ))
-                                }
+                                        </TableRow>
+                                {/*    ))*/}
+                                {/*}*/}
 
                             </TableBody>
                         </Table>
@@ -373,10 +351,9 @@ class ProductManage extends Component {
                     variant="filled"
 
                 />
-
             </Fragment>
         )
-    }
+}
 }
 
 export default ProductManage
